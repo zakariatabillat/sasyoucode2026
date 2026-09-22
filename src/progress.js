5557697c294ba2123d24d277efd3484d17770f83
zakaria tabillat <zakariatabillat@gmail.com>
2026-09-22 05:17:24 +0000

    added=enregisterresultat +ajouterapprenant

--- src/progress.js
+++ src/progress.js
@@ -25,3 +25,58 @@ function validerResultat(resultat) {
 }
 
 
+function enregistrerResultat(id, resultat) {
+
+    const student = studentsData.find(function (student) {
+        return student.id === id;
+    });
+
+    if (!student) {
+        return false;
+    }
+
+    if (!validerResultat(resultat)) {
+        return false;
+    }
+
+    const existingResult = student.results.find(function (result) {
+        return result.jour === resultat.jour;
+    });
+
+    if (existingResult) {
+        existingResult.exercicesTermines = resultat.exercicesTermines;
+        existingResult.totalExercices = resultat.totalExercices;
+        existingResult.challengeTermine = resultat.challengeTermine;
+
+        return true;
+    }
+
+    student.results.push(resultat);
+
+    return true;
+}
+
+
+
+function ajouterApprenant(id, name, city) {
+
+    const existingStudent = studentsData.find(function (student) {
+        return student.id === id;
+    });
+
+    if (existingStudent) {
+        return false;
+    }
+
+
+    studentsData.push({
+        id: id,
+        name: normaliserNom(name),
+        city: city,
+        results: []
+    });
+
+    return true;
+}
+
+
